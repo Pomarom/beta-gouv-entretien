c536606eeaca191b1e92bcdbcd06073a35c1a791
Paul Gaucher <paul@paulinformatique.com>
2023-11-28 19:56:24 +0100

    First modification : - Get all apprentissage mission with a status - Display paginated mission on react list

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import axios from 'axios'
+import cors from 'cors'
 
 const app: Express = express();
 const port = 8080;
 
+app.use(cors())
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
@@ -10,3 +13,15 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+app.get('/missions', async (req: Request, res: Response) => {
+  // First get only the statusrepponse and deduce the apprentissage mission only
+  // const startups = await axios.get('https://api.github.com/repos/betagouv/beta.gouv.fr/contents/content/_startups')
+  const statusResponse = await axios.get('https://api.github.com/repos/mission-apprentissage/upptime/contents/history/summary.json')
+
+  const jsonStatusb64 = statusResponse.data.content
+  const decodedB64 = Buffer.from(jsonStatusb64, "base64");
+  const jsonData = JSON.parse(decodedB64.toString());
+  const dataFormatted = jsonData.map(( { status, name, slug } : any) => ( { status, name, id: slug }))
+  res.send(dataFormatted);  
+})
